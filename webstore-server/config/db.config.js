cb623afb94d82b399d32b60a484cf5e71ed1d2ae
Frodewald <frodewaldgrimbert777@gmail.com>
2025-01-07 01:19:43 +0700

    taruh url ke env

--- webstore-server/config/db.config.js
+++ webstore-server/config/db.config.js
@@ -1,5 +1,8 @@
 require('dotenv').config();
 
 module.exports = {
-    url: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@wald-cluster-shard-00-00.wwy2c.mongodb.net:${process.env.DB_PORT},wald-cluster-shard-00-01.wwy2c.mongodb.net:${process.env.DB_PORT},wald-cluster-shard-00-02.wwy2c.mongodb.net:${process.env.DB_PORT}/${process.env.DB_NAME}?replicaSet=atlas-944cly-shard-0&ssl=true&authSource=admin`
+    url: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 }
+// module.exports = {
+//     url: `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@wald-cluster-shard-00-00.wwy2c.mongodb.net:${process.env.DB_PORT},wald-cluster-shard-00-01.wwy2c.mongodb.net:${process.env.DB_PORT},wald-cluster-shard-00-02.wwy2c.mongodb.net:${process.env.DB_PORT}/${process.env.DB_NAME}?replicaSet=atlas-944cly-shard-0&ssl=true&authSource=admin`
+// }
