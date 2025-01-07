092bcb8b0f9190ae74b3f8cee76104190b19460c
Frodewald <frodewaldgrimbert777@gmail.com>
2025-01-07 22:06:57 +0700

    tambah secure = production

--- webstore-server/app.js
+++ webstore-server/app.js
@@ -19,7 +19,7 @@ app.use(session({
     }),
     cookie: {
         httpOnly: true,
-        secure: false,
+        secure: process.env.NODE_ENV === 'production',
         maxAge: 1000 * 60 * 60 * 24,
         sameSite: 'none'
     }
