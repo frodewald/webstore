1ef8dcdbf4f4837c5d492906a5b67b399f80865b
Frodewald Grimbert <67095670+frodewald@users.noreply.github.com>
2025-01-07 14:36:14 +0700

    Delete webstore-server directory

--- webstore-server/app/routes/category.route.js
+++
@@ -1,8 +0,0 @@
-module.exports = (app) => {
-    const categories = require('../controllers/category.controller')
-    const router = require('express').Router()
-
-    router.get('/', categories.findAllCategory)
-
-    app.use('/api/categories', router)
-}
